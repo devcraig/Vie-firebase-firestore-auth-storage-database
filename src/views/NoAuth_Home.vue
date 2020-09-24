eaaa0314a07d06278f6477ae87b7ad15f2a54f9c
Devin Craig <devinfordcraig@gmail.com>
2020-09-24 19:29:17 -0400

    init

--- src/views/NoAuth_Home.vue
+++ src/views/NoAuth_Home.vue
@@ -1,12 +1,13 @@
 <template>
   <v-container>
     <v-layout>
-      <v-flex xs12 sm6 offset-sm3>
+      <v-flex xs12 sm6 offset-sm3 class="align-content-center flex-wrap">
         <img src="../assets/Concord_Logo_Final.png" style="height:216px;width:400px;">
         <v-card class="primary" style="width:400px;">
           <v-card-title primary-title>
            <div>
-              <h3 class="headline mb-0" style="color:white;">Welcome to Concord. Sign in or Sign Up</h3>
+              <p class="headline mb-0 text-center" style="color:white;">Welcome to Concord</p>
+              <p class="text-center" style="color:white;">Sign in or Sign Up</p>
                 <div>
                 </div>
             </div>
